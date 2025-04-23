889fce41b9bbcc65e6a1b5199a357caf582a85d0
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-24 03:19:23 +0530

    A B C D E F G H I J

--- pattern.cpp
+++ pattern.cpp
@@ -7,10 +7,10 @@ int main(){
     
     // printing pattern
     int i=1;
+    char ch ='A';
     while(i<=n){
         int j=1;
         while(j<=i){
-            char ch ='A'+i-j;
             cout<<ch<<" ";
             j++; 
             ch++;   
