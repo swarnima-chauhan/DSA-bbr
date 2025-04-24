e97fa773ec7ecf3d066f94b311ed3f6d2fe82051
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-24 20:36:09 +0530

    123  45   6

--- invertedtriangle2.cpp
+++ invertedtriangle2.cpp
@@ -6,6 +6,7 @@ int main(){
     cout<<"enter number :";
     cin>>n;
 
+    int count=1;
     for(int i=0;i<n;i++){
         if(i!=0){
             for(int j=0;j<i;j++){
@@ -14,7 +15,8 @@ int main(){
         }
         //stars
         for(int j=0;j<n-i;j++){
-            cout<<"*";
+            cout<<count;
+            count++;
         }
         cout<<endl;
     }
