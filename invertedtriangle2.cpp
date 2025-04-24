df4ad423e4208a8859f6b695e65f66d58c961e24
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-24 20:44:05 +0530

    inverted triangle

--- invertedtriangle2.cpp
+++ invertedtriangle2.cpp
@@ -8,13 +8,14 @@ int main(){
 
     int count=1;
     for(int i=0;i<n;i++){
-        if(i!=0){
-            for(int j=0;j<i;j++){
-                cout<<" ";
-            }
+        
+        //spaces
+        for(int j=0;j<n-i+1;j++){
+            cout<<" ";
         }
+    
         //stars
-        for(int j=0;j<n-i;j++){
+        for(int j=0;j<=i;j++){
             cout<<count;
             count++;
         }
